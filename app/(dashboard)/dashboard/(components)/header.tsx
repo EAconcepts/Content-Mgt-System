8194b15461e2e9ac85eb81022274b9cb90283763
EAconcepts <amooemmanuel10@gmail.com>
2024-01-18 04:05:45 +0100

    adjusted the dashboard mobile menu

--- app/(dashboard)/dashboard/(components)/header.tsx
+++ app/(dashboard)/dashboard/(components)/header.tsx
@@ -47,34 +47,33 @@ const Header = () => {
   const [activeLink, setActiveLink] = useState<string>('Dashboard')
   const menuRef:  any = useRef(null)
   const onOutsideClick = (event: any) => {
-    if (menuRef.current && !menuRef.current.contains(event?.target as Node)) {
+    if (openNav && menuRef.current && !menuRef.current.contains(event?.target as Node)) {
       setOpenNav(false);
     }
   };
+  
   useEffect(() => {
     document.addEventListener("click", onOutsideClick);
     return () => {
       document.removeEventListener("click", onOutsideClick);
     };
-  }, []);
+  }, [openNav]);
   return (
   <header className="w-full relative bg-[#E5E5E5] flex flex-col gap-y-[24px] py-[24px] px-[32px]">
     <div className='w-full flex justify-between'>
       <Image src={logo} width={0} height={0} alt="logo" className=""/>
       <div ref={menuRef} onClick={()=>{
         setOpenNav(true)
-        console.log('yeahhhhh')
-        console.log('yeahhhhh')
-        }}>
-      <RxHamburgerMenu size={24} />  
+        }}
+        className="">
+      <RxHamburgerMenu size={24}/>  
       </div>
     </div>
     <h4 className="text-[#143D3C] font-manrope font-[600] text-[20px] leading-[16px] ">Welcome Nesto,</h4>
     {
-      openNav &&
-    <nav  className="fixed top-0 bottom-0 right-0 z-[999] bg-white max-w-fit px-[10px] pt-[24px] flex flex-col">
+      // openNav &&
+    <nav ref={menuRef} className={`fixed top-0 bottom-0 right-0 z-[999] bg-white max-w-fit px-[24px] pt-[24px] flex flex-col transition-all ease-in duration-500 ${!openNav && 'right-[-100%]'}`}>
       <X onClick={()=>{
-        console.log('Cliked!!!')
         setOpenNav(false)}} className="self-end mr-[18px]"/>
       <div className='mt-[16px] flex gap-x-[8px]'>
         <Image src={avatar} width={0} height={0} alt="avatar" className=""/>
@@ -86,7 +85,7 @@ const Header = () => {
       <ul className="flex flex-col gap-y-[24px] mt-[32px]">
         {
           navlists?.map((item, index)=>((
-        <Link key={index} href={'#'} className={twJoin(`flex gap-x-[12px] items-center pl-[16px] py-[8px] text-[#808080] ${activeLink ===item.title && 'bg-secondary text-white'}`)}
+        <Link key={index} href={'#'} className={twJoin(`flex gap-x-[12px] items-center px-[26px] py-[8px] text-[#808080] ${activeLink ===item.title && 'bg-secondary text-white'}`)}
         onClick={()=>setActiveLink(item.title)}
         >
           {item.icon}
